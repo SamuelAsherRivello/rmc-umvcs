957c4c7330e96af7cb7cd3e20301b1a551bc3d07
srivello <srivello@hotmail.com>
2021-02-22 15:12:36 -0600

    Add new Test

---
+++ Unity/Assets/Scripts/Tests/Editor/RMC/Projects/MyBouncyBallExample/UMVCS/Model/MainModelTest.cs
@@ -0,0 +1,24 @@
+﻿using NUnit.Framework;
+
+namespace RMC.Projects.MyBouncyBallExample.UMVCS.Model
+{
+	/// <summary>
+	/// Test the public API of the <see cref="MainModel"/>.
+	/// </summary>
+	public class MainModelTest
+	{
+		[Test]
+		public void BounceCount_IsZero_BeforeSetter()
+		{
+			// Arrange
+			MainModel mainModel = new MainModel();
+
+			// Act
+			int bounceCount = mainModel.BounceCount.Value;
+
+			// Assert
+			Assert.That(bounceCount, Is.EqualTo(0));
+
+		}
+	}
+}
