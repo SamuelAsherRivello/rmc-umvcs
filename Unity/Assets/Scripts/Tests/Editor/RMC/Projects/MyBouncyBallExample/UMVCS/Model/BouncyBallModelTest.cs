7f3551aad26db58f970e659b5931473e5a9d16cd
srivello <srivello@hotmail.com>
2021-02-22 15:12:27 -0600

    Update BouncyBallModelTest.cs

--- Unity/Assets/Scripts/Tests/Editor/RMC/Projects/MyBouncyBallExample/UMVCS/Model/BouncyBallModelTest.cs
+++ Unity/Assets/Scripts/Tests/Editor/RMC/Projects/MyBouncyBallExample/UMVCS/Model/BouncyBallModelTest.cs
@@ -1,12 +1,28 @@
 ﻿using NUnit.Framework;
+using RMC.Projects.MyBouncyBallExample.UMVCS.Controller;
+using UnityEngine;
 
 namespace RMC.Projects.MyBouncyBallExample.UMVCS.Model
 {
 
+	/// <summary>
+	/// Test the public API of the <see cref="BouncyBallModelTest"/>.
+	/// </summary>
 	public class BouncyBallModelTest
 	{
+		private GameObject parentGameObject = null;
+
+		[TearDown]
+		public void OnTearDown()
+		{
+			if (parentGameObject != null)
+			{
+				GameObject.DestroyImmediate(parentGameObject, false);
+			}
+		}
+
 		[Test]
-		public void stateCount_IsZero_BeforeBlah()
+		public void StateCount_IsZero_BeforeInitializeStateMachine()
 		{
 			// Arrange
 			BouncyBallModel bouncyBallModel = new BouncyBallModel();
@@ -16,6 +32,23 @@ namespace RMC.Projects.MyBouncyBallExample.UMVCS.Model
 
 			// Assert
 			Assert.That(stateCount, Is.EqualTo(0));
+
+		}
+
+		[Test]
+		public void StateCount_Is2_AfterInitializeStateMachine()
+		{
+			// Arrange
+			parentGameObject = new GameObject("ParentGameObject");
+			BouncyBallController bouncyBallController = parentGameObject.AddComponent<BouncyBallController>();
+			BouncyBallModel bouncyBallModel = new BouncyBallModel();
+			
+			// Act
+			bouncyBallModel.InitializeStateMachine(bouncyBallController);
+			int stateCount = bouncyBallModel.StateMachine.States.Count;
+
+			// Assert
+			Assert.That(stateCount, Is.EqualTo(2));
 
 		}
 	}
