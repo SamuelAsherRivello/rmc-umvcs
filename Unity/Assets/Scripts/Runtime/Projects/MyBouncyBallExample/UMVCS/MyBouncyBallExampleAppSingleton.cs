22437a2cb116e6ec521420dcd6bc9697574c1fc1
srivello <srivello@hotmail.com>
2021-04-02 16:55:26 -0600

    Added MultiScene support
    Now load from "Main" or "Loader" and both work well.

---
+++ Unity/Assets/Scripts/Runtime/Projects/MyBouncyBallExample/UMVCS/MyBouncyBallExampleAppSingleton.cs
@@ -0,0 +1,15 @@
+﻿using RMC.DesignPatterns.Singleton;
+
+namespace RMC.Projects.MyBouncyBallExample.UMVCS
+{
+	/// <summary>
+	/// TODO: Add comment
+	/// </summary>
+	public class MyBouncyBallExampleAppSingleton : SingletonMonoBehaviour<MyBouncyBallExampleAppSingleton>
+	{
+		protected void Start()
+		{
+
+		}
+	}
+}
